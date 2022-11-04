77c546940d25e8c95918061d0072b14defaee444
Gabriel-Paes <paessouzagabriel@gmail.com>
2022-11-04 13:57:28 -0300

    Finished :tada:

--- src/App.java
+++ src/App.java
@@ -1,5 +1,50 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Pessoa people = new Pessoa();
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite seu Nome:");
+        people.setNome(sc.next());
+
+        System.out.println("Digite seu Sobrenome:");
+        people.setSobrenome(sc.next());
+
+        System.out.println("Digite sua data de Nascimento\n Dia:");
+        int d = sc.nextInt();
+
+        System.out.println(" Mês:");
+        int m = sc.nextInt();
+
+        System.out.println(" Ano:");
+        int a = sc.nextInt();
+
+        System.out.println("Digite sua altura:");
+        people.setAltura(sc.nextDouble());
+        
+        System.out.println("Digite seu peso:");
+        people.setPeso(sc.nextDouble());
+
+        sc.close();
+
+        Data dtNascimento = new Data(d,m,a);
+
+        people.CalculaIdade(dtNascimento);
+
+        people.CalculaIMC(people.getPeso(), people.getAltura());
+        
+        people.InformaObesidade(people.getImc());
+
+        String nomeCompleto = people.getNome() + " " + people.getSobrenome();
+
+        System.out.println("Nome completo: "+ nomeCompleto);
+        System.out.println("Nome de referência: "+ people.getSobrenome() + ", " + people.getNome().toUpperCase());
+        System.out.println("Idade: "+ people.getIdade());
+        System.out.println("Peso: "+ people.getPeso());
+        System.out.println("Altura: "+ people.getAltura());
+        System.out.println("IMC: "+ people.getImc());
+        System.out.println("Classifição: "+ people.getCalassificacaoCorporal());
     }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,4 +1,4 @@
-import java.sql.Date;
+import java.util.Calendar;
 
 public class Pessoa {
     private String nome;
@@ -8,7 +8,7 @@ public class Pessoa {
     private double peso;
     private double imc;
     private String calassificacaoCorporal;
-    private Data data_nascimento;
+    private Data data_nascimento = new Data();
 
     public Pessoa() {
         nome = "";
@@ -18,7 +18,9 @@ public class Pessoa {
         peso = 0.0;
         imc = 0.0;
         calassificacaoCorporal = "";
-        data_nascimento = 0;
+        data_nascimento.setDia(0);
+        data_nascimento.setMes(0);
+        data_nascimento.setAno(0);
     }
 
     public Pessoa(String nome, String sobrenome, int idade, double altura, double peso, double imc, String calassificacaoCorporal, Data data_nascimento){
@@ -79,35 +81,26 @@ public class Pessoa {
     public void setImc(double imc) {
         this.imc = imc;
     }
+    
+    public String getCalassificacaoCorporal() {
+        return calassificacaoCorporal;
+    }
 
-/*     public double CalculaIMC() {
-        peso = 0.0;
-        altura = 0.0;
-        imc = (peso/(Math.pow(altura, 2)));
-        return imc;
-    } */
+    public void setCalassificacaoCorporal(String calassificacaoCorporal) {
+        this.calassificacaoCorporal = calassificacaoCorporal;
+    }
+
+    public Data getData_nascimento() {
+        return data_nascimento;
+    }
+    
+    public void setData_nascimento(Data data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
 
     public double CalculaIMC(double peso, double altura) {
        return imc = (peso/(Math.pow(altura, 2)));
     }
-
-/*     public String InformaObesidade() {
-        imc = 0.0;
-        if (imc < 18.5) {
-            calassificacaoCorporal = "Abaixo do peso";
-        } else if ((imc >= 18.5) && (imc <= 24.9)){
-            calassificacaoCorporal = "Peso normal";
-        } else if ((imc >= 25) && (imc <= 29.9)){
-            calassificacaoCorporal = "Sobrepeso";
-        } else if ((imc >= 30) && (imc <= 34.9)){
-            calassificacaoCorporal = "Obesidade grau 1";
-        } else if ((imc >= 35) && (imc <= 39.9)){
-            calassificacaoCorporal = "Obesidade grau 2";
-        } else if ((imc >= 40)){
-            calassificacaoCorporal = "Obesidade grau 3";
-        }
-        return calassificacaoCorporal;
-    } */
 
     public String InformaObesidade(double imc) {
         if (imc < 18.5) {
@@ -126,8 +119,9 @@ public class Pessoa {
         return calassificacaoCorporal;
     }
 
-    public int CalculaIdade(Data data_nascimento, Date hoje){
-        return idade = (hoje - data_nascimento);
+    public int CalculaIdade(Data data_nascimento){
+        Calendar data_agora = Calendar.getInstance();
+        idade = (data_agora.get(Calendar.YEAR) - data_nascimento.getAno());
+        return idade;
     }
-
 }
